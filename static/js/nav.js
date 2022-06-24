e4e0c1b52cc91bc0ba89a8983890dff338b47d95
Kiru <kiruthegreat2@gmail.com>
2022-06-24 14:50:43 -0500

    Kirugaming.com Blog Update! - Moved everything to run on flask - Blog - Account system for blog writer - Tag and filtering system - Made blog good for mobile i hope

---
+++ static/js/nav.js
@@ -0,0 +1,9 @@
+// start page with dropdown content hidden
+document.getElementById("redirectDropdown").classList.toggle("hide");
+
+// display dropdown items
+function compressedRedirects() {
+    document.getElementById("redirectDropdown").classList.toggle("hide");
+    document.getElementById("redirectDropdown").classList.toggle("show");
+
+}
