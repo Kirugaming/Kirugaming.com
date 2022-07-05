8a36f78f483990e531c7978706e678247dec5fcd
Kiru <kiruthegreat2@gmail.com>
2022-07-05 13:29:25 -0500

    More secure entry writer Implmented flask-login Remembers login favicon fix

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
-from flask import Flask, render_template, request, redirect
+import flask_login
+from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, UserMixin
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
+app.secret_key = 'amongus'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 
+# database models
 class BlogEntries(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     tag = db.Column(db.String(200), nullable=False)
@@ -19,13 +26,25 @@ class BlogEntries(db.Model):
         return '<entry %r>' % self.id
 
 
-class Accounts(db.Model):
+class Accounts(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.Integer, nullable=False)
     password = db.Column(db.Integer, nullable=False)
 
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
     def __repr__(self):
         return '<account %r>' % self.id
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    user = Accounts.query.filter_by(id=user_id).first()
+    if user:
+        return user
+    return None
 
 
 @app.route('/')
@@ -64,10 +83,14 @@ def login():
             Accounts.query.filter_by(username=check_username).first().password, check_password):
         return "Wrong username or password"
 
+    user = Accounts.query.filter_by(username=check_username).first()
+    login_user(user)
+
     return redirect("/entrywriter")
 
 
 @app.route('/entrywriter', methods=["POST", "GET"])
+@flask_login.login_required
 def entryWriter():
     if request.method != "POST":
         return render_template("entryWriter.html")
